d686f45ba49eaf0586210c1a35848926014d704d
Fabio <fabioborgesouzajunior@gmail.com>
2023-12-12 23:25:13 -0300

    oAuth2 added

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-const http=require("http")
-const url=require("url")
-const fs=require("fs")
+require("dotenv").config()
+const http = require("http")
+const url = require("url")
+const fs = require("fs")
+const express=require("express")
+const session=require("express-session")
+const passport=require("passport")
+const Strategy=require("./node_modules/passport-discord/lib/strategy")
+const app=express()
 
-//create server http
-http.createServer((req, res)=>{
-    res.writeHead(200, {"content-type":"text/plain; charset=utf-8"})
-    const urlParsed=url.parse(req.url)
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
-    if(urlParsed.pathname=="/"){
-        
-    }
-}).listen("3000")
+var scopes = ['identify'];
+
+passport.use(new Strategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.REDIRECT_URI,
+  scope: scopes,
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+      return done(null, profile);
+  });
+}));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(express.static(__dirname + '/')); 
+
+app.get("/", (req,res)=>{
+  const html = fs.readFileSync("./src/html/index.html", "utf-8");
+  res.end(html);
+})
+
+app.get('/auth/discord', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
+
+app.get('/auth/discord/callback',
+    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/app') } // auth success
+);
+
+app.get('/app', checkAuth, function(req, res) {
+  const html = fs.readFileSync("./src/html/index.html", "utf-8");
+  res.end(html);
+});
+
+function checkAuth(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.send('not logged in :(');
+}
+
+
+app.listen(3000, function (err) {
+  if (err) return console.log(err)
+  console.log('Listening at http://localhost:3000/')
+})
--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-const CLIENT_ID="1183424074031046656"
-const REDIRECT_URI="http://localhost:3000/callback"
-
-
-document.getElementById("discord-login").addEventListener("click", ()=>{
-    window.location.href=`https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=identify%20email`
-})
