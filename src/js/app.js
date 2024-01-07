75a802c3eed7c96f6a192296b4d9c6c5fdf5edfe
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-07 15:45:53 -0300

    fetching data player

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,21 @@
+// Variables
 const avatarElement=document.getElementById("avatar")
 
-fetch("/player")
-.then((res)=>res.json())
-.then((data)=>{
-    const player=data.player
-    return avatarElement.innerHTML=`<img class="avatarImage" src="https://cdn.discordapp.com/avatars/${player.userIdDiscord}/${player.avatar}.png" alt="">`
-})
+// Functions
+function getPlayerData(){
+    return fetch("/player")
+    .then((res)=>res.json())
+    .then((data)=>data.player)
+    .catch((error)=>console.error(error))
+}
 
+function insertAvatar(){
+    getPlayerData()
+    .then((player)=>{
+        avatarElement.innerHTML=`<img class="avatarImage" src="https://cdn.discordapp.com/avatars/${player.userIdDiscord}/${player.avatar}.png" alt="">`
+    })
+    .catch((error)=> console.error(error))
+}
+
+// Init
+insertAvatar()
